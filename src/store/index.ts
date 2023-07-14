66ce1c4e9336ae11c7794e291ae1b942809b360b
lsp59862 <2505856331@qq.com>
2023-07-14 15:39:22 +0800

    feat: 中心仓库详情(除管理员外)

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,8 +3,9 @@ import useAppStore from './modules/app';
 import useUserStore from './modules/user';
 import useTabBarStore from './modules/tab-bar';
 import useOrderInfoStore from './modules/order_info';
+import useWarehouseStore from './modules/warehouse-info';
 
 const pinia = createPinia();
 
-export { useAppStore, useUserStore, useTabBarStore, useOrderInfoStore };
+export { useAppStore, useUserStore, useTabBarStore, useOrderInfoStore, useWarehouseStore};
 export default pinia;
---
+++ src/store/modules/warehouse-info/types.ts
@@ -0,0 +1,11 @@
+export interface WareHouseRecord {
+  id?: string;
+  name?: string;
+  adminName?: string;
+  adminEmail?: string;
+  province?: string;
+  city?: string;
+  district?: string;
+  totalNumber?: number;
+  useNumber?: number;
+}
